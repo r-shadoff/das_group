d778c12cc6b00b77addf81a50fbcf613c137fc44
r-shadoff <rachelshadoff@hotmail.ca>
2025-02-25 13:29:53 -0500

    Update main.sh
    Added volcano plot code

--- main.sh
+++ main.sh
@@ -3,6 +3,8 @@ library(tidyverse)
 library(dplyr)
 library(stringr)
 library(ggplot2)
+library(ggrepel)
+library(DESeq2)
 
 # ----- Import Data
 customized_read_tsv <- function(file){
@@ -94,11 +96,80 @@ high_variance_genes <- cancer_df_filtered %>%
   summarise(var_read_count = var(read_count)) %>% # calculate read count variance
   filter(var_read_count > variance_threshold) %>% # compares variance to threshold
   pull(gene_name) # pulls gene names that correspond with variances > threshold
-# ----- Plot 1
-# ----- Plot 2
 
-# ----- Table
+# Filter dataframe to only contain genes with high variance
+genes_df_filtered <- cancer_df_filtered %>% 
+  filter(gene_name %in% high_variance_genes)
 
-# ----- Plot 3
+genes_df_filtered <- genes_df_filtered %>%
+  group_by(gene_name, sample_id, day_number) %>% 
+  summarise(read_count=sum(read_count), .groups = "drop")# Ensures that for each gene, sample, and day number there is only one read count value
 
-print(length(high_variance_genes))
+# Create a count matrix
+count_matrix <- genes_df_filtered %>%
+  pivot_wider(names_from = c(sample_id, day_number), values_from= read_count) %>%
+  column_to_rownames("gene_name")
+
+# Create metadata object 
+metadata <- data.frame(
+  row.names = colnames(count_matrix), # Format is sample_day
+  sample_id = sub("_.*", "", colnames(count_matrix)),  # Extract sample id
+  day_number = as.factor(sub(".*_", "", colnames(count_matrix)))  # Extract day 
+)
+
+# Differential gene analysis
+
+# create Dseq2 object 
+dds <- DESeqDataSetFromMatrix(
+  countData = count_matrix,
+  colData = metadata,
+  design = ~day_number
+)
+
+# Run DESeq2
+dds <- DESeq(dds)
+
+# Store and sort results
+DESeq_results <- results(dds, contrast = c("day_number", "D0", "D6"))
+DESeq_results_sorted <- DESeq_results[order(DESeq_results$padj, na.last = TRUE), ] # Creates table with most sigificant genes at the top
+
+# Create Volcano Plot 
+DESeq_results_df <- as.data.frame(DESeq_results)
+DESeq_results_df <- subset(DESeq_results_df, !is.na(padj)) # Drops p values that are NA
+
+# Add significance column for colour coding plot
+DESeq_results_df <- DESeq_results_df %>%  mutate(significance = ifelse(padj < 0.05 & abs(log2FoldChange) > 1, "Significant", "Not Significant"))
+
+# Add expression labels for colour coding
+DESeq_results_df <- DESeq_results_df %>%   mutate(expression = case_when(
+  log2FoldChange > 1  ~ "Overexpressed",
+  log2FoldChange < -1 ~ "Underexpressed",
+  TRUE ~ "Neither" # TRUE = when log2fold is between -1 and 1
+))
+
+# Take the top 20 genes and label them on the volcano plot 
+most_sig_genes <- DESeq_results_df %>%
+  filter(padj<0.01 & abs(log2FoldChange) > 3) %>%
+  arrange(padj) %>%
+  slice_head(n =10) # Takes top 20 most significant genes 
+
+# Plot 1: Volcano Plot
+
+ggplot(DESeq_results_df, aes(x = log2FoldChange, y = -log10(padj), color = expression)) +
+  geom_point(alpha = 0.8, size = 2) +
+  scale_color_manual(values = c("Overexpressed" = "red", "Underexpressed" = "blue")) +
+  theme_minimal() +
+  labs(
+    title = "Volcano Plot",
+    x = "Log2 Fold Change",
+    y = "-Log10 Adjusted p-value",
+    colour = "Expression Status"
+  ) +
+  geom_vline(xintercept = c(-1, 1), linetype = "dashed", color = "black") +  # FC threshold lines
+  geom_hline(yintercept = -log10(0.05), linetype = "dashed", color = "black") + # p-value threshold line
+  geom_text_repel(data = most_sig_genes, aes(label = rownames(most_sig_genes)), size = 2, colour = 'black')
+
+# Plot 2
+# Plot 3? 
+# Table of GSEA results 
+
